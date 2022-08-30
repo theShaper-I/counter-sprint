70a8cf78eeb85213abdcfc6d1c72c3e6d870bfa4
Artem <example@gmail.com>
2022-08-30 11:13:04 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  }
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter container mx-auto">
+      <div className="">
+        <h2 className="text-center mt-20 text-3xl text-gray-500">Counter:</h2>
+        <h1 className="text-center mt-4 text-7xl"> {count}</h1>
+        <div className="flex justify-center mt-6">
+          <button onClick={onClickMinus} className="btn bg-red-700 rounded-xl px-4 py-2 text-white text-2xl mr-4 drop-shadow-lg">- Minus</button>
+          <button onClick={onClickPlus} className="btn bg-green-700 rounded-xl px-4 py-2 text-white text-2xl drop-shadow-lg">Plus +</button>
+        </div>
+
+      </div>
     </div>
   );
 }
